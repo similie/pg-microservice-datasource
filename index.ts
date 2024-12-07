62b7e56a2858538180a84d13c63b1e5727756bac
Adam Smith <adam.smith@similie.org>
2024-12-07 15:10:12 +0800

    version bump

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
 export * from "./lib";
 export * from "typeorm";
+export { RelationMetadata } from "typeorm/metadata/RelationMetadata";
+export { ColumnMetadata } from "typeorm/metadata/ColumnMetadata";
+export { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+export { PickKeysByType } from "typeorm/common/PickKeysByType";
